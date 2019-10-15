429ad00f2f0a582e1a0afd8067a8e30ef157c521
Hussein, Youssef <youssefmohamed552@gmail.com>
2019-10-15 13:13:31 -0400

    I can get a window

---
+++ build/CMakeFiles/openGL.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/youssefhussein/programming/cplusplus/openGL/src/main.cpp" "/Users/youssefhussein/programming/cplusplus/openGL/build/CMakeFiles/openGL.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "/usr/local/include/GLFW"
+  "../src"
+  "../include"
+  "/usr/local/Cellar/glfw/3.3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/openGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openGL.dir/src/main.cpp.o"
+  "openGL.pdb"
+  "openGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
